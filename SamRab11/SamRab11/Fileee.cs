fec0bd49f4f4c562e6b93ba79fd60cb5c4a2717e
j <112891364+jhellsing@users.noreply.github.com>
2022-09-28 10:36:24 +0300

    Add files via upload

---
+++ SamRab11/SamRab11/Fileee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace SamRab11
+{
+    public class Fileee
+    {
+        public async void fileee()
+        {
+            string path = @"C:\Users\Student.POLCOL\Desktop\егоровсуперегоровкласс.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ SamRab11/SamRab11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamRab11
+{
+    internal class Program : Fileee
+    {
+        public static void Main(string[] args)
+        {
+            Fileee file = new Fileee();
+            file.fileee();
+            Console.ReadLine();
+        }
+    }
+}
